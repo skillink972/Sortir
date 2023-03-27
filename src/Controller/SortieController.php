44d018c394325b1efa4b8785ef0f95b240761e0d
lguilbeau2022 <laurence.guilbeau@hotmail.fr>
2023-03-27 17:18:23 +0200

    Début affichage sorties avec filtres

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -4,31 +4,39 @@ namespace App\Controller;
 
 use App\Entity\Sortie;
 use App\Form\SortieType;
+use App\Repository\ParticipantRepository;
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/sortie', name:'sortie')]
 class SortieController extends AbstractController
 {
-    #[Route('/CreeSortie', name: 'app_CreeSortie')]
-    public function nouveau(): Response
-    {
-        $Sortie = new SortieController();
-        $SortieForm = $this->createForm(SortieType::class, $Sortie);
-        $SortieForm->handleRequest();
-        if ($SortieForm->isSubmitted() && $SortieForm->isValid()) {
-            try {
-                $Sortie->setNom(getNom());
-            } catch (\Exception $exception) {
-                $this->addFlash('echec', 'Le souhait n\'a pas été inséré');
 
-                return $this->redirectToRoute('app_CreeSortie');
-            }
+    #[Route('/Afficher', name: '_afficher')]
+    public function afficher(
+        SortieRepository $sortieRepository,
+        ParticipantRepository $participantRepository
+    ):Response {
+        $sortiesDontJeSuisOrganisateur = $sortieRepository->findBy(
+            [
+            "organisateur" => $this->getUser()->getUserIdentifier()
+            ]
+        );
+        $sortiesAuxquellesJeSuisInscrit = $sortieRepository->findBy(
+            [
+                "participant_id" => $this->getUser()->getUserIdentifier()
+            ]
+        );
+        $sortiesAuxquellesJeNeSuisPasInscrit = $sortieRepository->findBy(
+            [
+
+            ]
+        );
 
 
-            return $this->render('cree_sortie/index.html.twig', [
-                'controller_name' => 'SortieController',
-            ]);
-        }
+        return $this->render('sortie/afficher.html.twig');
     }
+
 }
