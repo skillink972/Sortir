25f5de8fe0db3a8932877f94b1c5381ddb0c0d68
jsportes2022 <sportes.jeremy@gmail.com>
2023-03-27 12:27:44 +0200

    Création connexion/création de compte

---
+++ src/Entity/Etat.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtatRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EtatRepository::class)]
+class Etat
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $libelle = null;
+
+    #[ORM\OneToMany(mappedBy: 'etat', targetEntity: Sortie::class)]
+    private Collection $Sorties;
+
+    public function __construct()
+    {
+        $this->Sorties = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Sortie>
+     */
+    public function getSorties(): Collection
+    {
+        return $this->Sorties;
+    }
+
+    public function addSorty(Sortie $sorty): self
+    {
+        if (!$this->Sorties->contains($sorty)) {
+            $this->Sorties->add($sorty);
+            $sorty->setEtat($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSorty(Sortie $sorty): self
+    {
+        if ($this->Sorties->removeElement($sorty)) {
+            // set the owning side to null (unless already changed)
+            if ($sorty->getEtat() === $this) {
+                $sorty->setEtat(null);
+            }
+        }
+
+        return $this;
+    }
+}
