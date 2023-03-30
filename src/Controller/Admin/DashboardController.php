152191bf6a61c515649cabfeabc9195799c3b6f8
jsportes2022 <sportes.jeremy@gmail.com>
2023-03-30 11:43:24 +0200

    création du tableau de bord admin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Lieu;
+use App\Entity\Participant;
+use App\Entity\Sortie;
+use App\Entity\Ville;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+       // return parent::index();
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        return $this->render('admin/index.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('SORTIR.com');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Participants', 'fas fa-users', Participant::class);
+        yield MenuItem::linkToCrud('Lieux', 'fas fa-list', Lieu::class);
+        yield MenuItem::linkToCrud('Sorties', 'fas fa-list', Sortie::class);
+        yield MenuItem::linkToCrud('Villes', 'fas fa-list', Ville::class);
+    }
+}
