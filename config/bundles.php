2ff5b1bb939186c8c67e0d6928f93b889e638c72
skillink972 <79556664+skillink972@users.noreply.github.com>
2023-04-05 09:41:12 +0200

    creation css

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
